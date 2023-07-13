996b9d880adf5e036b6658567f08e4fefb91bcdf
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-07-13 19:53:29 -0400

    Agregar archivos de proyecto.

---
+++ 20230713s/Contenido/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _20230713s.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _20230713s.Contenido
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Plato> Platos => Set<Plato>();//Plato será en nombrede la tabla tras la migración
+        public AppDbContext(DbContextOptions<AppDbContext> op) : base(op)
+        {
+            
+        }
+    }
+}
---
+++ 20230713s/Models/Plato.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _20230713s.Models
+{
+    public class Plato
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+    }
+}
