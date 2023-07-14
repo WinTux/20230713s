c4216c50397fdb8fcb653f3ea17e765b9122bb26
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-07-14 19:41:15 -0400

    Servidor listo

---
+++ 20230713s/Migrations/20230714225916_initialmigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _20230713s.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Platos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nombre = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platos", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Platos");
+        }
+    }
+}
--- 20230713s/Program.cs
+++ 20230713s/Program.cs
@@ -1,4 +1,32 @@
+using _20230713s.Contenido;
+using _20230713s.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>( op => op.UseSqlite(builder.Configuration.GetConnectionString("MiConexionLocalSQLite")));
 var app = builder.Build();
-
+app.MapGet("api/plato", async (AppDbContext contexto) => {
+    var elementos = await contexto.Platos.ToListAsync();
+    return Results.Ok(elementos);
+});
+app.MapPost("api/plato", async (AppDbContext contexto, Plato plato) => {
+    var elementos = await contexto.Platos.AddAsync(plato);
+    await contexto.SaveChangesAsync();
+    return Results.Created($"api/plato/{plato.id}", plato);//HTTP 201, URI y objeto
+});
+app.MapPut("api/plato/{cod}", async (AppDbContext contexto, int cod, Plato plato) => {
+    var platoModelo = await contexto.Platos.FirstOrDefaultAsync(p => p.id == cod);
+    if (platoModelo == null)
+        return Results.NotFound();// Error 404
+    platoModelo.nombre = plato.nombre;
+    await contexto.SaveChangesAsync();
+    return Results.NoContent(); // HTTP 204
+});
+app.MapDelete("api/plato/{cod}", async (AppDbContext contexto, int cod) => {
+    var platoModelo = await contexto.Platos.FirstOrDefaultAsync(p => p.id == cod);
+    if (platoModelo == null)
+        return Results.NotFound();// Error 404
+    contexto.Platos.Remove(platoModelo);
+    await contexto.SaveChangesAsync();
+    return Results.NoContent(); // HTTP 204
+});
 app.Run();
